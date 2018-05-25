350e09ae6464e1a34db5292e3a2004e164884bea
sgalal <sgalal.me@outlook.com>
2018-05-25 13:28:27 +0800

    Add n=8 support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # knights-tour-visualization
 Knight's tour visualization in C++ and JavaScript
 
-**Not** finished and it only supports the number `6` yet.
+**Not** finished and it only supports the number 6 and 8 yet.
--- tour.cpp
+++ tour.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+#include <array>
 #include <vector>
 #include <map>
 #include <utility>
@@ -44,7 +46,7 @@ static std::pair <Grid, Point> localize(int n, Point pos)
     return std::make_pair(std::make_pair(gridx, gridy), std::make_pair(x, y));
 }
 
-bool atEdge(int n, Point pos, std::pair <Grid, Point> grid)
+static bool atEdge(int n, Point pos, std::pair <Grid, Point> grid)
 {
     return (pos.first != 0 && pos.first != n-1 && pos.second != 0 && pos.second != n-1) &&
             (((grid.second.first == 0 || grid.second.first == grid.first.first-1) &&
@@ -53,31 +55,58 @@ bool atEdge(int n, Point pos, std::pair <Grid, Point> grid)
               (grid.second.first < 3 || grid.second.first >= grid.first.first-3)));
 }
 
-static const std::vector <std::vector <std::vector <Offset>>> Grid66 =
+static const std::array <const Offset *, 8> d =
 {
-    {{{2,1},{1,2}}, {{-1,2},{1,2}}, {{-2,1},{2,1}}, {{-2,1},{2,1}}, {{-2,1},{1,2}}, {{-2,1},{-1,2}}},
-    {{{2,-1},{2,1}}, {{2,-1},{-1,2}}, {{-2,-1},{2,-1}}, {{2,-1},{1,2}}, {{-2,-1},{-1,2}}, {{-2,-1},{-2,1}}},
-    {{{1,-2},{1,2}}, {{-1,-2},{2,1}}, {{-2,-1},{-1,-2}}, {{2,-1},{2,1}}, {{1,-2},{1,2}}, {{-1,-2},{-1,2}}},
-    {{{1,-2},{1,2}}, {{-1,2},{1,2}}, {{-2,1},{2,1}}, {{-2,-1},{1,-2}}, {{-1,-2},{1,2}}, {{-2,-1},{-1,2}}},
-    {{{2,-1},{2,1}}, {{-1,-2},{2,1}}, {{-2,1},{2,1}}, {{-2,1},{2,1}}, {{-2,-1},{1,-2}}, {{-1,-2}, {-2,1}}},
-    {{{1,-2},{2,-1}}, {{-1,-2},{2,-1}}, {{-1,-2},{-2,-1}}, {{-2,-1},{2,-1}}, {{-2,-1},{1,-2}}, {{-1,-2},{-2,-1}}}
+    new Offset(-2,-1),
+    new Offset(-1,-2),
+    new Offset(1,-2),
+    new Offset(2,-1),
+    new Offset(2,1),
+    new Offset(1,2),
+    new Offset(-1,2),
+    new Offset(-2,1)
 };
 
-static const std::map <Grid, const std::vector <std::vector <std::vector <Offset>>> &> m =
+static const std::vector <std::vector <std::vector <const Offset *>>> Grid66 =
 {
-    {{6,6}, Grid66}
+    {{d[4],d[5]}, {d[6],d[5]}, {d[7],d[4]}, {d[7],d[4]}, {d[7],d[5]}, {d[7],d[6]}},
+    {{d[3],d[4]}, {d[3],d[6]}, {d[0],d[3]}, {d[3],d[5]}, {d[0],d[6]}, {d[0],d[7]}},
+    {{d[2],d[5]}, {d[1],d[4]}, {d[0],d[1]}, {d[3],d[4]}, {d[2],d[5]}, {d[1],d[6]}},
+    {{d[2],d[5]}, {d[6],d[5]}, {d[7],d[4]}, {d[0],d[2]}, {d[1],d[5]}, {d[0],d[6]}},
+    {{d[3],d[4]}, {d[1],d[4]}, {d[7],d[4]}, {d[7],d[4]}, {d[0],d[2]}, {d[1],d[7]}},
+    {{d[2],d[3]}, {d[1],d[3]}, {d[1],d[0]}, {d[0],d[3]}, {d[0],d[2]}, {d[1],d[0]}}
+};
+
+static const std::vector <std::vector <std::vector <const Offset *>>> Grid88 =
+{
+    {{d[5],d[4]}, {d[5],d[6]}, {d[5],d[7]}, {d[4],d[7]}, {d[5],d[7]}, {d[4],d[7]}, {d[5],d[7]}, {d[6],d[7]}},
+    {{d[3],d[5]}, {d[3],d[6]}, {d[3],d[0]}, {d[3],d[6]}, {d[3],d[6]}, {d[3],d[0]}, {d[5],d[6]}, {d[0],d[7]}},
+    {{d[2],d[5]}, {d[1],d[6]}, {d[1],d[7]}, {d[1],d[5]}, {d[4],d[5]}, {d[1],d[3]}, {d[2],d[5]}, {d[1],d[6]}},
+    {{d[2],d[3]}, {d[1],d[5]}, {d[2],d[5]}, {d[2],d[4]}, {d[6],d[7]}, {d[2],d[4]}, {d[0],d[5]}, {d[1],d[6]}},
+    {{d[2],d[5]}, {d[1],d[6]}, {d[3],d[7]}, {d[4],d[5]}, {d[1],d[6]}, {d[0],d[1]}, {d[2],d[5]}, {d[0],d[1]}},
+    {{d[3],d[5]}, {d[5],d[6]}, {d[1],d[4]}, {d[1],d[2]}, {d[4],d[5]}, {d[0],d[5]}, {d[2],d[5]}, {d[1],d[6]}},
+    {{d[2],d[4]}, {d[1],d[4]}, {d[4],d[7]}, {d[2],d[7]}, {d[0],d[1]}, {d[4],d[7]}, {d[0],d[7]}, {d[1],d[7]}},
+    {{d[2],d[3]}, {d[1],d[3]}, {d[0],d[1]}, {d[0],d[3]}, {d[0],d[3]}, {d[1],d[3]}, {d[1],d[2]}, {d[1],d[0]}}
+};
+
+static const std::map <Grid, const std::vector <std::vector <std::vector <const Offset *>>> &> m =
+{
+    {{6,6}, Grid66},
+    {{8,8}, Grid88}
 };
 
 static Point nextPoint(int n, Point pos, Point lastPos)
 {
     std::pair <Grid, Point> ret = localize(n, pos);
-    const std::vector <std::vector <std::vector <Offset>>> &grid = m.at(ret.first);
-    const std::vector <Offset> &v = grid[ret.second.second][ret.second.first];
-    const Offset &o1 = v[0];
-    const Offset &o2 = atEdge(n, pos, ret) ? v[2] : v[1];
-    Point newpoint1 = std::make_pair(pos.first+o1.first, pos.second+o1.second);
-    Point newpoint2 = std::make_pair(pos.first+o2.first, pos.second+o2.second);
-    return lastPos == newpoint1 ? newpoint2 : newpoint1;
+    const std::vector <std::vector <std::vector <const Offset *>>> &grid = m.at(ret.first);
+    const std::vector <const Offset *> &v = grid[ret.second.second][ret.second.first];
+    const Offset *o1 = v[0];
+    const Offset *o2 = atEdge(n, pos, ret) ? v[2] : v[1];
+    Point newpoint1 = std::make_pair(pos.first+o1->first, pos.second+o1->second);
+    Point newpoint2 = std::make_pair(pos.first+o2->first, pos.second+o2->second);
+    if(lastPos == newpoint1) return newpoint2;
+    else if(lastPos == newpoint2) return newpoint1;
+    assert(0);
 }
 
 static Point make_point(int x, int y)
