ad58e6660bfab707c6346dc70d41641b23664db5
sgalal <sgalal.me@outlook.com>
2018-05-25 16:03:46 +0800

    Use indices instead of pointers

--- tour.h
+++ tour.h
@@ -64,29 +64,29 @@ static const std::array <const Offset *, 8> d =
     new Offset(-2,1)
 };
 
-static const std::vector <std::vector <std::vector <const Offset *>>> Grid66 =
+static const std::vector <std::vector <std::vector <unsigned int>>> Grid66 =
 {
-    {{d[4],d[5]}, {d[6],d[5]}, {d[7],d[4]}, {d[7],d[4]}, {d[7],d[5]}, {d[7],d[6]}},
-    {{d[3],d[4]}, {d[3],d[6]}, {d[0],d[3]}, {d[3],d[5]}, {d[0],d[6]}, {d[0],d[7]}},
-    {{d[2],d[5]}, {d[1],d[4]}, {d[0],d[1]}, {d[3],d[4]}, {d[2],d[5]}, {d[1],d[6]}},
-    {{d[2],d[5]}, {d[6],d[5]}, {d[7],d[4]}, {d[0],d[2]}, {d[1],d[5]}, {d[0],d[6]}},
-    {{d[3],d[4]}, {d[1],d[4]}, {d[7],d[4]}, {d[7],d[4]}, {d[0],d[2]}, {d[1],d[7]}},
-    {{d[2],d[3]}, {d[1],d[3]}, {d[1],d[0]}, {d[0],d[3]}, {d[0],d[2]}, {d[1],d[0]}}
+    {{4,5}, {6,5}, {7,4}, {7,4}, {7,5}, {7,6}},
+    {{3,4}, {3,6}, {0,3}, {3,5}, {0,6}, {0,7}},
+    {{2,5}, {1,4}, {0,1}, {3,4}, {2,5}, {1,6}},
+    {{2,5}, {6,5}, {7,4}, {0,2}, {1,5}, {0,6}},
+    {{3,4}, {1,4}, {7,4}, {7,4}, {0,2}, {1,7}},
+    {{2,3}, {1,3}, {1,0}, {0,3}, {0,2}, {1,0}}
 };
 
-static const std::vector <std::vector <std::vector <const Offset *>>> Grid88 =
+static const std::vector <std::vector <std::vector <unsigned int>>> Grid88 =
 {
-    {{d[5],d[4]}, {d[5],d[6]}, {d[5],d[7]}, {d[4],d[7]}, {d[5],d[7]}, {d[4],d[7]}, {d[5],d[7]}, {d[6],d[7]}},
-    {{d[3],d[5]}, {d[3],d[6]}, {d[3],d[0]}, {d[3],d[6]}, {d[3],d[6]}, {d[3],d[0]}, {d[5],d[6]}, {d[0],d[7]}},
-    {{d[2],d[5]}, {d[1],d[6]}, {d[1],d[7]}, {d[1],d[5]}, {d[4],d[5]}, {d[1],d[3]}, {d[2],d[5]}, {d[1],d[6]}},
-    {{d[2],d[3]}, {d[1],d[5]}, {d[2],d[5]}, {d[2],d[4]}, {d[6],d[7]}, {d[2],d[4]}, {d[0],d[5]}, {d[1],d[6]}},
-    {{d[2],d[5]}, {d[1],d[6]}, {d[3],d[7]}, {d[4],d[5]}, {d[1],d[6]}, {d[0],d[1]}, {d[2],d[5]}, {d[0],d[1]}},
-    {{d[3],d[5]}, {d[5],d[6]}, {d[1],d[4]}, {d[1],d[2]}, {d[4],d[5]}, {d[0],d[5]}, {d[2],d[5]}, {d[1],d[6]}},
-    {{d[2],d[4]}, {d[1],d[4]}, {d[4],d[7]}, {d[2],d[7]}, {d[0],d[1]}, {d[4],d[7]}, {d[0],d[7]}, {d[1],d[7]}},
-    {{d[2],d[3]}, {d[1],d[3]}, {d[0],d[1]}, {d[0],d[3]}, {d[0],d[3]}, {d[1],d[3]}, {d[1],d[2]}, {d[1],d[0]}}
+    {{5,4}, {5,6}, {5,7}, {4,7}, {5,7}, {4,7}, {5,7}, {6,7}},
+    {{3,5}, {3,6}, {3,0}, {3,6}, {3,6}, {3,0}, {5,6}, {0,7}},
+    {{2,5}, {1,6}, {1,7}, {1,5}, {4,5}, {1,3}, {2,5}, {1,6}},
+    {{2,3}, {1,5}, {2,5}, {2,4}, {6,7}, {2,4}, {0,5}, {1,6}},
+    {{2,5}, {1,6}, {3,7}, {4,5}, {1,6}, {0,1}, {2,5}, {0,1}},
+    {{3,5}, {5,6}, {1,4}, {1,2}, {4,5}, {0,5}, {2,5}, {1,6}},
+    {{2,4}, {1,4}, {4,7}, {2,7}, {0,1}, {4,7}, {0,7}, {1,7}},
+    {{2,3}, {1,3}, {0,1}, {0,3}, {0,3}, {1,3}, {1,2}, {1,0}}
 };
 
-static const std::map <Grid, const std::vector <std::vector <std::vector <const Offset *>>> &> m =
+static const std::map <Grid, const std::vector <std::vector <std::vector <unsigned int>>> &> m =
 {
     {{6,6}, Grid66},
     {{8,8}, Grid88}
@@ -95,10 +95,10 @@ static const std::map <Grid, const std::vector <std::vector <std::vector <const 
 static Point nextPoint(int n, Point pos, Point lastPos)
 {
     std::pair <Grid, Point> ret = localize(n, pos);
-    const std::vector <std::vector <std::vector <const Offset *>>> &grid = m.at(ret.first);
-    const std::vector <const Offset *> &v = grid[ret.second.second][ret.second.first];
-    const Offset *o1 = v[0];
-    const Offset *o2 = atEdge(n, pos, ret) ? v[2] : v[1];
+    const std::vector <std::vector <std::vector <unsigned int>>> &grid = m.at(ret.first);
+    const std::vector <unsigned int> &v = grid[ret.second.second][ret.second.first];
+    const Offset *o1 = d[v[0]];
+    const Offset *o2 = atEdge(n, pos, ret) ? d[v[2]] : d[v[1]];
     Point newpoint1 = std::make_pair(pos.first+o1->first, pos.second+o1->second);
     Point newpoint2 = std::make_pair(pos.first+o2->first, pos.second+o2->second);
     if(lastPos == newpoint1) return newpoint2;
