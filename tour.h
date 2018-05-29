c6f3741e5e93aca3f66320717c1e1fbab767f5db
sgalal <sgalal.me@outlook.com>
2018-05-29 15:27:30 +0800

    Optimize C++

--- tour.h
+++ tour.h
@@ -3,12 +3,13 @@
 #include <vector>
 #include <map>
 #include <utility>
+#include <algorithm>
 
 using Grid = std::pair <int, int>;
 using Point = std::pair <int, int>;
 using Offset = std::pair <int, int>;
 
-static const std::array <const Offset, 8> d =
+static const std::array <const Offset, 8> dir =
 {
     Offset(-2,-1), Offset(-1,-2),
     Offset( 1,-2), Offset( 2,-1),
@@ -16,7 +17,7 @@ static const std::array <const Offset, 8> d =
     Offset(-1, 2), Offset(-2, 1)
 };
 
-static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid66 =
+static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid6_6 =
 {
     {{4,5}, {5,6}, {4,7}, {4,7}, {5,7}, {6,7}},
     {{3,4}, {3,6}, {0,3}, {3,5}, {0,6}, {0,7}},
@@ -26,7 +27,7 @@ static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid66 =
     {{2,3}, {1,3}, {1,0}, {3,0}, {0,2}, {0,1}}
 };
 
-static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid86 =
+static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid8_6 =
 {
     {{4,5}, {4,6}, {4,7}, {4,7}, {4,6}, {4,6}, {5,7}, {6,7}},
     {{3,4}, {3,6}, {6,0}, {0,4}, {0,3}, {0,3}, {0,5}, {0,7}},
@@ -36,7 +37,7 @@ static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid86 =
     {{2,3}, {1,2}, {0,2}, {0,2}, {2,3}, {0,3}, {1,2}, {0,1}}
 };
 
-static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid88 =
+static const std::vector <std::vector <std::array <unsigned int, 2>>> Grid8_8 =
 {
     {{4,5}, {5,6}, {5,7}, {4,7}, {5,7}, {4,7}, {5,7}, {6,7}},
     {{3,5}, {3,6}, {0,3}, {3,6}, {3,6}, {0,3}, {5,6}, {0,7}},
@@ -94,24 +95,27 @@ static const std::array <int, 8> pathAfter = {4, 3, 7, 4, 0, 7, 3, 0};
 
 static const std::map <Grid, const std::vector <std::vector <std::array <unsigned int, 2>>> &> m =
 {
-    {{6,6}, Grid66},
-    {{8,6}, Grid86},
-    {{8,8}, Grid88},
+    {{6,6}, Grid6_6},
+    {{8,6}, Grid8_6},
+    {{8,8}, Grid8_8},
     {{10,8}, Grid10_8},
     {{10,10}, Grid10_10},
     {{12,10}, Grid12_10}
 };
 
+/// Returns the next point of the knight's tour.
+/// @param n Board size.
+/// @param posX X-axis of current position.
+/// @param posY Y-axis of current position.
+/// @param lastPosX X-axis of last position.
+/// @param lastPosY Y-axis of last position.
 static Point nextPoint(int n, int posX, int posY, int lastPosX, int lastPosY)
 {
-    Grid grid(n, n);
-    Point point;
+    int gridSizeX = n, gridSizeY = n;
+    int gridLocX = posX, gridLocY = posY;
     int pointAttribute = -1;
 
     {
-        int x = posX;
-        int y = posY;
-
         int blockChoiceX = 0;
         int blockChoiceY = 0;
 
@@ -134,48 +138,45 @@ static Point nextPoint(int n, int posX, int posY, int lastPosX, int lastPosY)
             }
         };
 
-        while(std::min(grid.first, grid.second) > 10 ||
-              std::max(grid.first, grid.second) > 12)
+        for(int min, max;;)
         {
-            process(grid.first, x, blockChoiceX);
-            process(grid.second, y, blockChoiceY);
+            std::tie(min, max) = std::minmax(gridSizeX, gridSizeY);
+            if(min <= 10 && max <= 12) break;
+
+            process(gridSizeX, gridLocX, blockChoiceX);
+            process(gridSizeY, gridLocY, blockChoiceY);
             
             assert(!((!blockChoiceX)^(!blockChoiceY)));
             pointAttribute =
-                blockChoiceX == 1 && blockChoiceY == 1 && (x == grid.first-3 && y == grid.second-1) ? 0 :
-                blockChoiceX == 1 && blockChoiceY == 1 && (x == grid.first-1 && y == grid.second-2) ? 1 :
-                blockChoiceX == 2 && blockChoiceY == 1 && (x == 1            && y == grid.second-3) ? 2 :
-                blockChoiceX == 2 && blockChoiceY == 1 && (x == 0            && y == grid.second-1) ? 3 :
-                blockChoiceX == 2 && blockChoiceY == 2 && (x == 2            && y == 0            ) ? 4 :
-                blockChoiceX == 2 && blockChoiceY == 2 && (x == 0            && y == 1            ) ? 5 :
-                blockChoiceX == 1 && blockChoiceY == 2 && (x == grid.first-2 && y == 2            ) ? 6 :
-                blockChoiceX == 1 && blockChoiceY == 2 && (x == grid.first-1 && y == 0            ) ? 7 : pointAttribute;
+                blockChoiceX == 1 && blockChoiceY == 1 && gridLocX == gridSizeX-3 && gridLocY == gridSizeY-1 ? 0 :
+                blockChoiceX == 1 && blockChoiceY == 1 && gridLocX == gridSizeX-1 && gridLocY == gridSizeY-2 ? 1 :
+                blockChoiceX == 2 && blockChoiceY == 1 && gridLocX == 1           && gridLocY == gridSizeY-3 ? 2 :
+                blockChoiceX == 2 && blockChoiceY == 1 && gridLocX == 0           && gridLocY == gridSizeY-1 ? 3 :
+                blockChoiceX == 2 && blockChoiceY == 2 && gridLocX == 2           && gridLocY == 0           ? 4 :
+                blockChoiceX == 2 && blockChoiceY == 2 && gridLocX == 0           && gridLocY == 1           ? 5 :
+                blockChoiceX == 1 && blockChoiceY == 2 && gridLocX == gridSizeX-2 && gridLocY == 2           ? 6 :
+                blockChoiceX == 1 && blockChoiceY == 2 && gridLocX == gridSizeX-1 && gridLocY == 0           ? 7 : pointAttribute;
         }
-
-        point = std::make_pair(x, y);
     }
+    
+    unsigned int h0, h1;
 
-    bool shouldReverse = grid.first < grid.second;
-
-    if(shouldReverse)
+    if(gridSizeX >= gridSizeY)
     {
-        std::swap(grid.first, grid.second);
-        std::swap(point.first, point.second);
+        const std::array <unsigned int, 2> &v = m.at(std::make_pair(gridSizeX, gridSizeY))[gridLocY][gridLocX];
+        h0 = v[0];
+        h1 = v[1];
     }
-
-    const std::array <unsigned int, 2> &v = m.at(grid)[point.second][point.first];
-
-    unsigned int h0 = v[0];
-    unsigned int h1 = v[1];
-
-    if(shouldReverse)
+    else
     {
-        h0 = (9-h0)%8;
-        h1 = (9-h1)%8;
+        const std::array <unsigned int, 2> &v = m.at(std::make_pair(gridSizeY, gridSizeX))[gridLocX][gridLocY];
+        h0 = (9-v[0])%8;
+        h1 = (9-v[1])%8;
     }
 
     if(pointAttribute != -1)
     {
+        assert(pointAttribute >= 0);
         int pori = pathOrigin[pointAttribute];
         int paft = pathAfter[pointAttribute];
         if(h0 == pori) h0 = paft;
@@ -183,8 +184,8 @@ static Point nextPoint(int n, int posX, int posY, int lastPosX, int lastPosY)
         else assert(0);
     }
 
-    Point newpoint0 = std::make_pair(posX + d[h0].first, posY + d[h0].second);
-    Point newpoint1 = std::make_pair(posX + d[h1].first, posY + d[h1].second);
+    Point newpoint0 = std::make_pair(posX + dir[h0].first, posY + dir[h0].second);
+    Point newpoint1 = std::make_pair(posX + dir[h1].first, posY + dir[h1].second);
 
     if(lastPosX == newpoint0.first && lastPosY == newpoint0.second) return newpoint1;
     else if(lastPosX == newpoint1.first && lastPosY == newpoint1.second) return newpoint0;
